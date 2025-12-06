3f3b2029478136a5940983fbf5ae32155613dcf6
Leonardo Alves da Silva <leonardo.silva1795@etec.sp.gov.br>
2025-12-05 23:20:36 -0300

    Vite arrumado

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,10 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/portfolio/',  // ← COM barra no início e fim
+  base: '/portfolio/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+    outDir: 'dist'
   }
 })
-=======
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-      base: './',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
->>>>>>> ddbc517d8a4ac43f4c4cc1d6de70cbec52fe8a47
