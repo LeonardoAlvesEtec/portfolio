ada05f2727e0c518aaf461ed7e8f7e53debdf309
Leonardo Alves da Silva <leonardo.silva1795@etec.sp.gov.br>
2025-12-05 23:42:56 -0300

    Último Vite

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
   base: '/portfolio/',
   build: {
     outDir: 'dist'
+  },
+  preview: {
+    port: process.env.PORT || 3000,
+    host: true,
+    allowedHosts: [
+      'portfolio-5-s5wp.onrender.com',
+      '.onrender.com',
+      'localhost'
+    ]
   }
 })
